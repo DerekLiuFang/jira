d42e2fc3ee1240adf2a513c75f73cef830563107
DerekLiuFang <derekliufang@outlook.com>
2021-02-25 22:52:00 +0800

    feat: 安装配置devtool

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,10 +1,25 @@
 import { FormEvent } from "react";
 
 export const LoginScreen = () => {
+  const login = (params: { username: string; password: string }) => {
+    fetch(`http://localhost:3001/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(params),
+    }).then(async (response) => {
+      if (response.ok) {
+      }
+    });
+  };
+
   const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-    // const username = (evt.currentTarget.elements[0] as HTMLInputElement).value;
-    // const password = (evt.currentTarget.elements[1] as HTMLInputElement).value;
+    const username = (evt.currentTarget.elements[0] as HTMLInputElement).value;
+    const password = (evt.currentTarget.elements[1] as HTMLInputElement).value;
+
+    login({ username, password });
   };
 
   return (
@@ -15,8 +30,9 @@ export const LoginScreen = () => {
       </div>
       <div>
         <label htmlFor="password">密码</label>
-        <input type="text" id={"password"} />
+        <input type="password" id={"password"} />
       </div>
+      <button type="submit">注册</button>
     </form>
   );
 };
